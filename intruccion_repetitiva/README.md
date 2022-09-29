e1e79477aace8dc13fecafd56f7d9fa19320fe1e
Heber Daniel Maluendas Lizarazo <111641836+Maluendas0510@users.noreply.github.com>
2022-09-29 09:58:44 -0500

    Add files via upload

---
+++ intruccion_repetitiva/README.md
@@ -0,0 +1,3 @@
+# suma de los 10 primeros numeros
+
+![diagrama de flujo](suma_n_numeros.png "diagrama de flujo")
---
+++ intruccion_repetitiva/repetitiva.py
@@ -0,0 +1,21 @@
+#suma de los 10 primeros numeros
+
+print("--------------------")
+print("-------suma---------")
+print("--------------------")
+
+#input
+n=10
+i=1
+sum=0
+
+#processing
+while(i<=n):
+    sum=sum+i
+    i=i+1
+        
+#fin
+print("-----resultado------")
+print("la suma es:" +str(sum))
+
+    
